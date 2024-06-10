6ae232e8288758932855525d8cc6301180efe5b0
li1ht2ay <li1ht2ay@users.noreply.github.com>
2024-06-10 04:45:23 +0000

    Update libretro.c

--- src/mappers/mapper268.c
+++ src/mappers/mapper268.c
@@ -98,11 +98,15 @@ static DECLFR(Mapper268_ReadWRAM) {
 =======
 static uint8 Mapper268_ReadWRAM(uint32 A) {
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 33ce3d0 (Update libretro.c)
 	return A001B &0xA0? CartBR(A): X.DB;
 =======
 	return A001B &0xA0? CartBR(A): cpu.DB;
 >>>>>>> eb0d00f (Rename X6502 X to cpu)
+=======
+	return A001B &0xA0? CartBR(A): cpu.openbus;
+>>>>>>> 363cf5e (Rename CPU.DB to cpu.openbus and rename ops.h to x6502ops.inc)
 }
 
 static void Mapper268_WriteWRAM(uint32 A, uint8 V) {
