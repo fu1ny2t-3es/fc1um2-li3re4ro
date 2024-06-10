fc185c28f3bb0ad08ff55ed70f13090245f0e9c2
li1ht2ay <li1ht2ay@users.noreply.github.com>
2024-06-10 04:45:25 +0000

    Update Makefile.libretro

--- src/mappers/mapper268.c
+++ src/mappers/mapper268.c
@@ -97,8 +97,12 @@ void Mapper268_MirrorWrap(uint8 V) {
 static DECLFR(Mapper268_ReadWRAM) {
 =======
 static uint8 Mapper268_ReadWRAM(uint32 A) {
+<<<<<<< HEAD
 >>>>>>> ae063eb (Update Makefile.libretro)
 	return A001B &0xA0? CartBR(A): X.DB;
+=======
+	return A001B &0xA0? CartBR(A): cpu.DB;
+>>>>>>> c2fdd1f (Rename X6502 X to cpu)
 }
 
 static void Mapper268_WriteWRAM(uint32 A, uint8 V) {
