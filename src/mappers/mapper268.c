db2a5eb39d3db89705abbd2a54909aaa3879f53a
li1ht2ay <li1ht2ay@users.noreply.github.com>
2024-06-10 04:45:30 +0000

    Change PLATFORM_SUPPORTS_ references to FRONTEND_SUPPORTS_

--- src/mappers/mapper268.c
+++ src/mappers/mapper268.c
@@ -97,8 +97,12 @@ void Mapper268_MirrorWrap(uint8 V) {
 static DECLFR(Mapper268_ReadWRAM) {
 =======
 static uint8 Mapper268_ReadWRAM(uint32 A) {
+<<<<<<< HEAD
 >>>>>>> eefddd1 (Change PLATFORM_SUPPORTS_ references to FRONTEND_SUPPORTS_)
 	return A001B &0xA0? CartBR(A): X.DB;
+=======
+	return A001B &0xA0? CartBR(A): cpu.DB;
+>>>>>>> 4152142 (Rename X6502 X to cpu)
 }
 
 static void Mapper268_WriteWRAM(uint32 A, uint8 V) {
