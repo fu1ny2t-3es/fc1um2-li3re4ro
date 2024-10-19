82f929c5efc9b9469050545d4ee2491975b64eb4
fu1ny2t <fu1ny2t@users.noreply.github.com>
2024-10-19 15:48:26 +0000

    Update libretro_core_options.h

--- src/palette.h
+++ src/palette.h
@@ -6,8 +6,12 @@ typedef struct {
 } pal;
 
 extern pal *palo;
+<<<<<<< HEAD
 extern uint8 palette_game_available;
 void FCEU_ResetPalette(void);
+=======
+extern int ipalette;
+>>>>>>> 6638d4c (Updates)
 
 void FCEU_ResetPalette(void);
 void FCEU_LoadGamePalette(void);
