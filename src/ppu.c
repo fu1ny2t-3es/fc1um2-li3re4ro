f6d155034e07cf1d52332d456888f3b1daf298d2
fu1ny2t <fu1ny2t@users.noreply.github.com>
2026-05-07 17:23:50 +0000

    Update libretro_core_options.h

--- src/ppu.c
+++ src/ppu.c
@@ -1508,7 +1508,7 @@ static void CopySprites(uint8_t *target) {
 >>>>>>> 53d29944 ((ppu.c) Cleanups)
 void FCEUPPU_SetVideoSystem(int w) {
 	if (w) {
-		scanlines_per_frame = dendy ? 262 : 312;
+		scanlines_per_frame = isDendy ? 262 : 312;
 		FSettings.FirstSLine = FSettings.UsrFirstSLine[1];
 		FSettings.LastSLine = FSettings.UsrLastSLine[1];
 	} else {
