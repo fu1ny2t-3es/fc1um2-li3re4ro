529dbfff8140c82bd86bfc7d50b68ec4b65f00fb
fu1ny2t <fu1ny2t@users.noreply.github.com>
2026-05-07 17:23:12 +0000

    Update libretro_core_options.h

--- src/mappers/mapper010.c
+++ src/mappers/mapper010.c
@@ -22,10 +22,9 @@
 
 #include "mapinc.h"
 
-static uint8_t is10, isPC10;
-static uint8_t creg[4], latch0, latch1, preg, mirr;
-static uint8_t *WRAM = NULL;
-static uint32_t WRAMSIZE;
+static uint8 creg[4], latch0, latch1, preg, mirr;
+static uint8 *WRAM = NULL;
+static uint32 WRAMSIZE;
 
 static SFORMAT StateRegs[] =
 {
@@ -38,27 +37,15 @@ static SFORMAT StateRegs[] =
 };
 
 static void Sync(void) {
-	if (is10) {
-		setprg16(0x8000, preg);
-		setprg16(0xC000, ~0);
-	} else {
-		setprg8(0x8000, preg);
-		setprg8(0xA000, ~2);
-		setprg8(0xC000, ~1);
-		setprg8(0xE000, ~0);
-	}
-	if (is10 || isPC10)
-		setprg8r(0x10, 0x6000, 0);
+	setprg16(0x8000, preg);
+	setprg16(0xC000, ~0);
+	setprg8r(0x10, 0x6000, 0);
 	setchr4(0x0000, creg[latch0]);
 	setchr4(0x1000, creg[latch1 + 2]);
 	setmirror(mirr);
 }
 
-<<<<<<< HEAD
-static DECLFW(MMC2and4Write) {
-=======
 static void MMC2and4Write(uint32 A, uint8 V) {
->>>>>>> 3cd7a223 (Update libretro_core_options.h)
 	switch (A & 0xF000) {
 	case 0xA000: preg = V & 0xF; Sync(); break;
 	case 0xB000: creg[0] = V & 0x1F; Sync(); break;
@@ -69,13 +56,8 @@ static void MMC2and4Write(uint32 A, uint8 V) {
 	}
 }
 
-<<<<<<< HEAD
-static void FP_FASTAPASS(1) MMC2and4PPUHook(uint32_t A) {
-	uint8_t l, h = A >> 8;
-=======
 static void MMC2and4PPUHook(uint32 A) {
 	uint8 l, h = A >> 8;
->>>>>>> 3cd7a223 (Update libretro_core_options.h)
 	if (h >= 0x20 || ((h & 0xF) != 0xF))
 		return;
 	l = A & 0xF0;
@@ -102,21 +84,14 @@ static void MMC2and4Power(void) {
 	preg = 0;
 	latch0 = latch1 = 1;
 	Sync();
-	if (is10 || isPC10) {
-		SetReadHandler(0x6000, 0x7FFF, CartBR);
-		SetWriteHandler(0x6000, 0x7FFF, CartBW);
-		FCEU_CheatAddRAM(WRAMSIZE >> 10, 0x6000, WRAM);
-	}
+	SetReadHandler(0x6000, 0x7FFF, CartBR);
+	SetWriteHandler(0x6000, 0x7FFF, CartBW);
+	FCEU_CheatAddRAM(WRAMSIZE >> 10, 0x6000, WRAM);
 	SetReadHandler(0x8000, 0xFFFF, CartBR);
 	SetWriteHandler(0xA000, 0xFFFF, MMC2and4Write);
 }
 
 static void StateRestore(int version) {
-	/* latch0/latch1 are written as boolean (0 or 1) and used as
-	 * indices into creg[4] via creg[latch0] and creg[latch1 + 2].
-	 * Clamp savestate-loaded values to keep the indices in range. */
-	latch0 &= 1;
-	latch1 &= 1;
 	Sync();
 }
 
@@ -126,35 +101,12 @@ static void MMC2and4Close(void) {
 	WRAM = NULL;
 }
 
-void Mapper9_Init(CartInfo *info) {
-	is10 = 0;
-	isPC10 = 0;
-	info->Power = MMC2and4Power;
-	info->Close = MMC2and4Close;
-	PPU_hook = MMC2and4PPUHook;
-	if (info->battery) { /* Mike Tyson's Punch-Out!! (PC10) supports save ram */
-		isPC10 = 1;
-		WRAMSIZE = 8192;
-		WRAM = (uint8_t*)FCEU_gmalloc(WRAMSIZE);
-		SetupCartPRGMapping(0x10, WRAM, WRAMSIZE, 1);
-		AddExState(WRAM, WRAMSIZE, 0, "WRAM");
-		if (info->battery) {
-			info->SaveGame[0] = WRAM;
-			info->SaveGameLen[0] = WRAMSIZE;
-		}
-	}
-	GameStateRestore = StateRestore;
-	AddExState(&StateRegs, ~0, 0, 0);
-}
-
 void Mapper10_Init(CartInfo *info) {
-	is10 = 1;
-	isPC10 = 0;
 	info->Power = MMC2and4Power;
 	info->Close = MMC2and4Close;
 	PPU_hook = MMC2and4PPUHook;
 	WRAMSIZE = 8192;
-	WRAM = (uint8_t*)FCEU_gmalloc(WRAMSIZE);
+	WRAM = (uint8*)FCEU_gmalloc(WRAMSIZE);
 	SetupCartPRGMapping(0x10, WRAM, WRAMSIZE, 1);
 	AddExState(WRAM, WRAMSIZE, 0, "WRAM");
 	if (info->battery) {
