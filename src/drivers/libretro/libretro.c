894ab335a33d17fc26d67d51cf39785419854732
fu1ny2t <fu1ny2t@users.noreply.github.com>
2026-05-07 17:23:11 +0000

    Update libretro_core_options.h

--- src/drivers/libretro/libretro.c
+++ src/drivers/libretro/libretro.c
@@ -3498,7 +3498,6 @@ void retro_cheat_set(unsigned index, bool enabled, const char *code)
       if ((codepart_len == 7) && (codepart[4]==':'))
       {
          /* raw code in xxxx:xx format */
-         log_cb.log(RETRO_LOG_DEBUG, "Cheat code added: '%s' (Raw)\n", codepart);
          codepart[4] = '\0';
          a = strtoul(codepart, NULL, 16);
          v = strtoul(codepart + 5, NULL, 16);
@@ -3511,7 +3510,6 @@ void retro_cheat_set(unsigned index, bool enabled, const char *code)
       else if ((codepart_len == 10) && (codepart[4] == '?') && (codepart[7] == ':'))
       {
          /* raw code in xxxx?xx:xx */
-         log_cb.log(RETRO_LOG_DEBUG, "Cheat code added: '%s' (Raw)\n", codepart);
          codepart[4] = '\0';
          codepart[7] = '\0';
          a = strtoul(codepart, NULL, 16);
@@ -3525,12 +3523,10 @@ void retro_cheat_set(unsigned index, bool enabled, const char *code)
       else if (GGisvalid(codepart) && FCEUI_DecodeGG(codepart, &a, &v, &c))
       {
          FCEUI_AddCheat(name, a, v, c, type);
-         log_cb.log(RETRO_LOG_DEBUG, "Cheat code added: '%s' (GG)\n", codepart);
       }
       else if (FCEUI_DecodePAR(codepart, &a, &v, &c, &type))
       {
          FCEUI_AddCheat(name, a, v, c, type);
-         log_cb.log(RETRO_LOG_DEBUG, "Cheat code added: '%s' (PAR)\n", codepart);
       }
       else
          log_cb.log(RETRO_LOG_DEBUG, "Invalid or unknown code: '%s'\n", codepart);
@@ -4138,6 +4134,7 @@ bool retro_load_game(const struct retro_game_info *info)
          frontend_post_load_init);
 
    if (!GameInfo)
+<<<<<<< HEAD
    {
 #ifdef HAVE_HDPACK
       HDNes_Unload();
@@ -4159,8 +4156,9 @@ bool retro_load_game(const struct retro_game_info *info)
        * will report that content loading has failed */
       FCEUD_DispMessage(RETRO_LOG_ERROR, 3000, "ROM loading failed...");
 #endif
+=======
+>>>>>>> 9f095e24 (Remove some DEBUG ifdefs)
       return false;
-   }
 
    /* Piggyback the libretro-side turbo toggle phase into the core's
     * savestate (#75).  FCEUI_LoadGame just finished populating SFMDATA[]
