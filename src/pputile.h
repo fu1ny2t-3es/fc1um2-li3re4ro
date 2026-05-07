ea549460ee3ddb90ae78f23c2c9c2c7760bc1326
fu1ny2t <fu1ny2t@users.noreply.github.com>
2026-05-07 17:23:49 +0000

    Update libretro_core_options.h

--- src/pputile.h
+++ src/pputile.h
@@ -5,9 +5,14 @@ uint32_t vadr;
 #ifndef PPUT_MMC5SP
 	uint8_t zz;
 #else
+<<<<<<< HEAD
 	uint8_t xs, ys;
 	xs = X1;
 	ys = ((scanline >> 3) + MMC5HackSPScroll) & 0x1F;
+=======
+	uint8 xs = X1;
+	uint8 ys = ((scanline >> 3) + MMC5HackSPScroll) & 0x1F;
+>>>>>>> 74f678f9 (Some general cleanups/removal of unused functions)
 	if (ys >= 0x1E) ys -= 0x1E;
 #endif
 
