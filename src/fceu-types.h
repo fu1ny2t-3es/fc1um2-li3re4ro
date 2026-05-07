4ded3732270ae2903749cc36970ede44621674e8
fu1ny2t <fu1ny2t@users.noreply.github.com>
2026-05-07 17:23:38 +0000

    Update ppu.c

--- src/fceu-types.h
+++ src/fceu-types.h
@@ -57,6 +57,7 @@ typedef uint64_t uint64;
 =======
 >>>>>>> e98261e5 (Update ppu.c)
 
+<<<<<<< HEAD
 #ifdef __GNUC__
 typedef unsigned long long uint64;
 typedef long long int64;
@@ -95,6 +96,10 @@ typedef uint8 (*readfunc)(uint32 A);
 =======
 #define FCEU_UNUSED(x)    (void)(x)
 #define FCEU_MAYBE_UNUSED __attribute__((unused))
+=======
+typedef uint64_t uint64;
+typedef int64_t int64;
+>>>>>>> 0fd19ae9 (Buildfixes for MSVC/C89)
 
 #if !defined(FALSE)
 #define FALSE 0
