fa732363bed9cb96648ad62c1d332fb9d63227f6
fu1ny2t <fu1ny2t@users.noreply.github.com>
2026-05-07 17:23:48 +0000

    Update libretro_core_options.h

--- src/fceu-endian.h
+++ src/fceu-endian.h
@@ -3,6 +3,14 @@
 
 #include "fceu-memory.h"
 
+<<<<<<< HEAD
 uint32_t FCEU_de32lsb(const uint8_t *);
+=======
+int write32le_mem(uint32 b, memstream_t *mem);
+int read32le_mem(uint32 *Bufo, memstream_t *mem);
+
+void FCEU_en32lsb(uint8 *, uint32);
+uint32 FCEU_de32lsb(const uint8 *);
+>>>>>>> 9c0002ac (Refactors)
 
 #endif
