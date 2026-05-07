20f25e0e775205aee872baddf1ed9aeaaea9bb3d
fu1ny2t <fu1ny2t@users.noreply.github.com>
2026-05-07 17:23:32 +0000

    Update Makefile.libretro

--- src/ppu.c
+++ src/ppu.c
@@ -143,10 +143,18 @@ void FP_FASTAPASS(1) (*PPU_hook)(uint32_t A);
 void (*PPU_hook)(uint32 A);
 >>>>>>> f6efdc94 (Update Makefile.libretro)
 
+<<<<<<< HEAD
 uint8_t vtoggle = 0;
 uint8_t XOffset = 0;
 
 uint32_t TempAddr = 0, RefreshAddr = 0;
+=======
+static uint8 vtoggle = 0;
+static uint8 XOffset = 0;
+
+static uint32 TempAddr = 0;
+uint32 RefreshAddr = 0;
+>>>>>>> 6d21fb01 ((PPU) Turn some variables static)
 
 static int maxsprites = 8;
 
