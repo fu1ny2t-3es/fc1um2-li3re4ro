dac1af933583794c4e813dd434c8d4e24d97a9b5
fu1ny2t <fu1ny2t@users.noreply.github.com>
2026-05-07 17:23:44 +0000

    Change PLATFORM_SUPPORTS_ references to FRONTEND_SUPPORTS_

--- src/fceu-types.h
+++ src/fceu-types.h
@@ -57,6 +57,7 @@ typedef uint64_t uint64;
 =======
 >>>>>>> 8bf4e730 (Change PLATFORM_SUPPORTS_ references to FRONTEND_SUPPORTS_)
 
+<<<<<<< HEAD
 #ifdef __GNUC__
 typedef unsigned long long uint64;
 typedef long long int64;
@@ -95,6 +96,10 @@ typedef uint8 (*readfunc)(uint32 A);
 =======
 #define FCEU_UNUSED(x)    (void)(x)
 #define FCEU_MAYBE_UNUSED __attribute__((unused))
+=======
+typedef uint64_t uint64;
+typedef int64_t int64;
+>>>>>>> bab0c822 (Buildfixes for MSVC/C89)
 
 #if !defined(FALSE)
 #define FALSE 0
