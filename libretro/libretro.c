3c69790d5c45789275f01da67aebb585009053ea
fu1ny2t <fu1ny2t@users.noreply.github.com>
2026-05-07 17:23:38 +0000

    Update ppu.c

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -469,11 +469,14 @@ void FCEUD_DispMessage(enum retro_log_level level, unsigned duration, const char
    }
 }
 
+<<<<<<< HEAD
 static void FCEUD_SoundToggle (void)
 {
    FSettings.SoundVolume = sndvolume;
 }
 
+=======
+>>>>>>> 41fa0e1b (Remove FCEUD_SoundToggle)
 /*palette for FCEU*/
 #define PAL_INTERNAL sizeof(palettes) / sizeof(palettes[0]) /* Number of palettes in palettes[] */
 #define PAL_DEFAULT  (PAL_INTERNAL + 1)
@@ -2667,7 +2670,7 @@ static void check_variables(bool startup)
    {
       int val = (int)(atof(var.value) * 25.6);
       sndvolume = val;
-      FCEUD_SoundToggle();
+      FSettings.SoundVolume = sndvolume;
    }
 
    if (audio_video_updated && !startup)
@@ -4248,7 +4251,7 @@ bool retro_load_game(const struct retro_game_info *info)
    current_palette = 0;
 
    ResetPalette();
-   FCEUD_SoundToggle();
+   FSettings.SoundVolume = sndvolume;
    check_variables(true);
    stereo_filter_init();
    PowerNES();
