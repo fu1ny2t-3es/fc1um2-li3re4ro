34a15961a69c373128a27707a29b6e6d01531394
fu1ny2t <fu1ny2t@users.noreply.github.com>
2026-05-07 17:23:25 +0000

    Update libretro.c

--- src/state.c
+++ src/state.c
@@ -639,8 +639,11 @@ void AddExState(void *v, uint32_t s, int type, char *desc)
    SFEXINDEX++;
    SFMDATA[SFEXINDEX].v = 0;   /* End marker. */
 }
+<<<<<<< HEAD
 
 void FCEU_DrawSaveStates(uint8_t *XBuf)
 {
 }
 
+=======
+>>>>>>> ca287e70 (Remove FCEU_DrawSaveStates)
