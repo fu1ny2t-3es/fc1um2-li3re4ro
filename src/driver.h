4590018272e035525a1e9bcb959fd233f7d7b013
fu1ny2t <fu1ny2t@users.noreply.github.com>
2026-06-19 17:31:34 +0000

    Update libretro_core_options.h

--- src/driver.h
+++ src/driver.h
@@ -187,8 +187,11 @@ int FCEUI_FDSInsert(int oride);
 int FCEUI_FDSEject(void);
 void FCEUI_FDSSelect(void);
 
+<<<<<<< HEAD
 int FCEUI_DatachSet(uint8_t *rcode);
 
+=======
+>>>>>>> 6ebeb523 (Split up more mappers)
 #ifdef  __cplusplus
 }
 #endif
