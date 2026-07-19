56e997aa744966644c5d89ddc3b5e98c885eede1
fu1ny2t <fu1ny2t@users.noreply.github.com>
2026-07-19 16:33:35 +0000

    Update libretro_core_options.h

--- src/fceu.h
+++ src/fceu.h
@@ -21,6 +21,7 @@ extern unsigned swapDuty; /* Swap bits 6 & 7 of $4000/$4004 to mimic bug
                            * found on some famiclones/Dendy models.
                            */
 
+<<<<<<< HEAD
 void ResetGameLoaded(void);
 
 <<<<<<< HEAD
@@ -34,6 +35,8 @@ void FASTAPASS(3) SetWriteHandler(int32_t start, int32_t end, writefunc func);
 writefunc FASTAPASS(1) GetWriteHandler(int32_t a);
 readfunc FASTAPASS(1) GetReadHandler(int32_t a);
 =======
+=======
+>>>>>>> e4eab013 (Simplify some functions)
 void FCEU_MemoryRand(uint8 *ptr, uint32 size);
 void SetReadHandler(int32 start, int32 end, readfunc func);
 void SetWriteHandler(int32 start, int32 end, writefunc func);
