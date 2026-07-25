16c5cd40756d46575b0b0604f661168ad16530a6
fu1ny2t <fu1ny2t@users.noreply.github.com>
2026-07-25 16:32:06 +0000

    Update ppu.c

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,6 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 CORE_DIR := $(LOCAL_PATH)/../src
+LINK_T := $(LOCAL_PATH)/../libretro/link.T
 
 HAVE_NTSC := 1
 
@@ -18,8 +19,12 @@ LOCAL_MODULE    := retro
 LOCAL_SRC_FILES := $(SOURCES_C)
 LOCAL_CFLAGS    := $(COREFLAGS)
 <<<<<<< HEAD
+<<<<<<< HEAD
 LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/drivers/libretro/link.T,-z,max-page-size=16384
 =======
 LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/libretro/link.T
 >>>>>>> 3752be2b ((Android) Fix build)
+=======
+LOCAL_LDFLAGS   := -Wl,-version-script=$(LINK_T)
+>>>>>>> 0cd1c82b (Update ppu.c)
 include $(BUILD_SHARED_LIBRARY)
