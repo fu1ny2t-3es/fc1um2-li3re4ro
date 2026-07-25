ed83c42224a758e5001e373adfd2eb623865030d
fu1ny2t <fu1ny2t@users.noreply.github.com>
2026-07-25 16:32:09 +0000

    Change PLATFORM_SUPPORTS_ references to FRONTEND_SUPPORTS_

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,6 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 CORE_DIR := $(LOCAL_PATH)/../src
+LINK_T := $(LOCAL_PATH)/../libretro/link.T
 
 HAVE_NTSC := 1
 
@@ -18,8 +19,12 @@ LOCAL_MODULE    := retro
 LOCAL_SRC_FILES := $(SOURCES_C)
 LOCAL_CFLAGS    := $(COREFLAGS)
 <<<<<<< HEAD
+<<<<<<< HEAD
 LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/drivers/libretro/link.T,-z,max-page-size=16384
 =======
 LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/libretro/link.T
 >>>>>>> 185b2a8e ((Android) Fix build)
+=======
+LOCAL_LDFLAGS   := -Wl,-version-script=$(LINK_T)
+>>>>>>> b771de51 (Change PLATFORM_SUPPORTS_ references to FRONTEND_SUPPORTS_)
 include $(BUILD_SHARED_LIBRARY)
