d85a292f240ad382de8342dc35b267406f68617e
fu1ny2t <fu1ny2t@users.noreply.github.com>
2026-08-25 16:17:20 +0000

    Update libretro.c

--- src/state.h
+++ src/state.h
@@ -25,8 +25,24 @@
 
 #include "fceu-memory.h"
 
+<<<<<<< HEAD
 int FCEUSS_Load_Mem(const void *buf, size_t size);
+=======
+<<<<<<< HEAD
+void FCEUSS_Load_Mem(const void *buf, size_t size);
+>>>>>>> 4b6e8beb (Update libretro.c)
 size_t FCEUSS_Save_Mem(void *buf, size_t size);
+=======
+/* last known fceu-mm version */
+#define FCEU_VERSION_MAJOR 0
+#define FCEU_VERSION_MINOR 98
+#define FCEU_VERSION_PATCH 13
+
+#define FCEU_VERSION_NUMERIC ((FCEU_VERSION_MAJOR * 10000) + (FCEU_VERSION_MINOR * 100) + (FCEU_VERSION_PATCH))
+
+void FCEUSS_Load_Mem(void);
+void FCEUSS_Save_Mem(void);
+>>>>>>> e167cfae (Update libretro.c)
 
 typedef struct {
 	void *v;
